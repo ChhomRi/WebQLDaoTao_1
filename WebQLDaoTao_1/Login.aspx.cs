7679b4f22fa35ed1f7c417c5b9747c1af0ddbf97
Chhom Ri <chhomri02@gmail.com>
2025-02-24 23:08:24 +0700

    tk

---
+++ WebQLDaoTao_1/Login.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebQLDaoTao_1
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btLogin_Click(object sender, EventArgs e)
+        {
+            bool check = true;
+            if (check)
+            {
+                Session["TaiKhoan"] = "doituongtaikhoan";
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ WebQLDaoTao_1/SecurePage.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SecurePage.aspx.cs" Inherits="WebQLDaoTao_1.SecurePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
